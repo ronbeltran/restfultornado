23e11b2f618ede78451e91b0c5de4f5375c102ba
Ronnie Beltran <rbbeltran.09@gmail.com>
2013-10-21 02:25:02 +0800

    add tests for event handlers

--- tests.py
+++ tests.py
@@ -10,6 +10,14 @@ class TestCase(unittest.TestCase):
         i = requests.get("http://localhost:8080/")
         assert i.status_code == 200
 
+    def test_events_get(self):
+        i = requests.get("http://localhost:8080/api/v1/events")
+        assert i.status_code == 403
+
+    def test_events_post(self):
+        i = requests.post("http://localhost:8080/api/v1/events/1/event-name")
+        assert i.status_code == 403
+
 
 if __name__=="__main__":
     unittest.main()
