f931726a1b13b1dea2b34a08794d842b2af29eb9
Ronnie Beltran <rbbeltran.09@gmail.com>
2013-10-21 02:15:40 +0800

    add event api handlers

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("Hello, world")
 
 
+class EventApiHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        raise tornado.web.HTTPError(403)
+
+
+class EventApiSaveHandler(tornado.web.RequestHandler):
+
+    def post(self, uid, event):
+        raise tornado.web.HTTPError(403)
+
+
 settings = {
     "title": u"Restful Json Api",
     "debug": os.environ.get('SERVER_SOFTWARE', '').startswith('Dev'),
@@ -19,4 +31,6 @@ settings = {
 
 application = tornado.wsgi.WSGIApplication([
     (r"/", MainHandler),
+    (r"/api/v1/events", EventApiHandler),
+    (r"/api/v1/events/([0-9]+)/(\w+)", EventApiSaveHandler),
 ], **settings)
