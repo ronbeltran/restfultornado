bb511b46027c0d90aa24c0e57c27c77d1c2f6d4d
Ronnie Beltran <rbbeltran.09@gmail.com>
2013-10-22 00:52:34 +0800

    add test for event post not found

--- tests.py
+++ tests.py
@@ -18,6 +18,10 @@ class TestCase(unittest.TestCase):
         i = requests.post("http://localhost:8080/api/v1/events/1/event1")
         assert i.status_code == 200
 
+    def test_events_post_not_found(self):
+        i = requests.post("http://localhost:8080/api/v1/events/100/event2")
+        assert i.status_code == 404
+
 
 if __name__=="__main__":
     unittest.main()
