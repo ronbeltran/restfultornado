965c95e8ebf5599985cfd0706b2ccc4f66f155bd
Ronnie Beltran <rbbeltran.09@gmail.com>
2013-10-21 23:28:28 +0800

    add initial users

--- models.py
+++ models.py
@@ -11,3 +11,21 @@ class Event(db.Model):
     user_id = db.ReferenceProperty(User)
     eventname = db.StringProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
+
+
+def create_users():
+
+    buzz = User(id=1, username="buzz")
+    buzz.put()
+
+    woody = User(id=2, username="woody")
+    woody.put()
+
+    lenny = User(id=3, username="lenny")
+    lenny.put()
+
+    squeeze = User(id=4, username="squeeze")
+    squeeze.put()
+
+    wheezy = User(id=5, username="wheezy")
+    wheezy.put()
