7eb73633bb39b467e193a34f78a1861c73c9c627
Ronnie Beltran <rbbeltran.09@gmail.com>
2013-10-24 23:16:46 +0800

    add now arg

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 import json
 from datetime import datetime, timedelta
 
-def timedelta_wrapper(time, delta=0):
+def timedelta_wrapper(time, delta=0, now=datetime.now()):
     """ This method is a wrapper for built in method timedelta() 
     since we don't know what the request argument time period.
     Arguments:    
     time is any of the following (hours, days, weeks, months) 
     delta is any value supplied to timedelta() eg. timedelta(hours=3)
     """
-    now = datetime.now() 
     # check if time period is None
     if not time:
         # will return sensible value timedelta(hours=0)
