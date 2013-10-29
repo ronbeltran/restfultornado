06bffccc631c873eca7792136d0668b3959ec90c
Ronnie Beltran <rbbeltran.09@gmail.com>
2013-10-30 01:28:43 +0800

    add EventCount model and fix by hour initial data

--- models.py
+++ models.py
@@ -22,14 +22,18 @@ class Event(db.Model):
                     created=str(self.created.isoformat()))
 
 
+class EventCount(db.Model):
+    user = db.ReferenceProperty(User, collection_name='user_event_count')
+    name = db.StringProperty(required=True)
+    count = db.IntegerProperty(default=0)
+
+    def to_dict(self):
+        return dict(user_id=int(self.user.id),
+                    name=str(self.name),
+                    count=str(self.count()))
+
+
 def initialize_db():
-
-    one_month_ago = datetime.now() - timedelta(weeks=4)
-    two_month_ago = datetime.now() - timedelta(weeks=8)
-
-    one_week_ago = datetime.now() - timedelta(weeks=1)
-    two_week_ago = datetime.now() - timedelta(weeks=2)
-    three_week_ago = datetime.now() - timedelta(weeks=3)
 
     one_day_ago = datetime.now() - timedelta(days=1)
     two_day_ago = datetime.now() - timedelta(days=2)
@@ -38,63 +42,63 @@ def initialize_db():
     five_day_ago = datetime.now() - timedelta(days=5)
     six_day_ago = datetime.now() - timedelta(days=6)
 
-    one_minute_ago = datetime.now() - timedelta(minutes=1)
-    two_minute_ago = datetime.now() - timedelta(minutes=2)
-    five_minute_ago = datetime.now() - timedelta(minutes=5)
-    ten_minute_ago = datetime.now() - timedelta(minutes=10)
-    fifteen_minute_ago = datetime.now() - timedelta(minutes=15)
-    thirty_minute_ago = datetime.now() - timedelta(minutes=30)
+    one_hour_ago = datetime.now() - timedelta(minutes=1)
+    two_hour_ago = datetime.now() - timedelta(minutes=2)
+    five_hour_ago = datetime.now() - timedelta(minutes=5)
+    ten_hour_ago = datetime.now() - timedelta(minutes=10)
+    fifteen_hour_ago = datetime.now() - timedelta(minutes=15)
+    thirty_hour_ago = datetime.now() - timedelta(minutes=30)
 
     # For buzz
     buzz = User(id=1)
     buzz.put()
 
-    e1 = Event(user=buzz, name="e1", created=one_day_ago)
+    e1 = Event(user=buzz, name="e1", created=one_hour_ago)
     e1.put()
 
-    e11 = Event(user=buzz, name="e1", created=one_day_ago)
+    e11 = Event(user=buzz, name="e1", created=one_hour_ago)
     e11.put()
 
-    e111 = Event(user=buzz, name="e1", created=one_day_ago)
+    e111 = Event(user=buzz, name="e1", created=one_hour_ago)
     e111.put()
 
-    e2 = Event(user=buzz, name="e2", created=one_day_ago)
+    e2 = Event(user=buzz, name="e2", created=one_hour_ago)
     e2.put()
 
-    e22 = Event(user=buzz, name="e2", created=one_day_ago)
+    e22 = Event(user=buzz, name="e2", created=two_hour_ago)
     e22.put()
 
-    e222 = Event(user=buzz, name="e2", created=one_day_ago)
+    e222 = Event(user=buzz, name="e2", created=two_hour_ago)
     e222.put()
 
-    e3 = Event(user=buzz, name="e3", created=two_day_ago)
+    e3 = Event(user=buzz, name="e3", created=five_hour_ago)
     e3.put()
 
-    e33 = Event(user=buzz, name="e3", created=two_day_ago)
+    e33 = Event(user=buzz, name="e3", created=five_hour_ago)
     e33.put()
 
-    e333 = Event(user=buzz, name="e3", created=two_day_ago)
+    e333 = Event(user=buzz, name="e3", created=five_hour_ago)
     e333.put()
 
-    e4 = Event(user=buzz, name="e4", created=two_day_ago)
+    e4 = Event(user=buzz, name="e4", created=ten_hour_ago)
     e4.put()
 
-    e44 = Event(user=buzz, name="e4", created=two_day_ago)
+    e44 = Event(user=buzz, name="e4", created=ten_hour_ago)
     e44.put()
 
-    e444 = Event(user=buzz, name="e4", created=two_day_ago)
+    e444 = Event(user=buzz, name="e4", created=ten_hour_ago)
     e444.put()
 
-    e5 = Event(user=buzz, name="e5", created=three_day_ago)
+    e5 = Event(user=buzz, name="e5", created=two_hour_ago)
     e5.put()
 
-    e55 = Event(user=buzz, name="e5", created=three_day_ago)
+    e55 = Event(user=buzz, name="e5", created=two_hour_ago)
     e55.put()
 
-    e555 = Event(user=buzz, name="e5", created=three_day_ago)
+    e555 = Event(user=buzz, name="e5", created=two_hour_ago)
     e555.put()
 
-    e6 = Event(user=buzz, name="e6", created=three_day_ago)
+    e6 = Event(user=buzz, name="e6", created=two_hour_ago)
     e6.put()
 
     e66 = Event(user=buzz, name="e6", created=three_day_ago)
@@ -107,11 +111,11 @@ def initialize_db():
     woody = User(id=2)
     woody.put()
 
-    e10 = Event(user=woody, name="e1", created=one_day_ago)
+    e10 = Event(user=woody, name="e1", created=one_hour_ago)
     e10.put()
 
-    e100 = Event(user=woody, name="e1", created=one_day_ago)
+    e100 = Event(user=woody, name="e1", created=one_hour_ago)
     e100.put()
 
-    e1000 = Event(user=woody, name="e2", created=one_day_ago)
+    e1000 = Event(user=woody, name="e2", created=two_hour_ago)
     e1000.put()
